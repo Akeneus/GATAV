738d4dd18c656f29f9136c7af8d5ba142621ebc1
Dennis Schwartz <dennis.schwartz.privat@gmail.com>
2019-05-05 19:09:44 +0200

    V1.0 BackgroundLayer

---
+++ app/src/main/java/com/example/gatavprojekt_001/Background_Layer/MapHandler.java
@@ -0,0 +1,295 @@
+package com.example.gatavprojekt_001.Background_Layer;
+
+import android.util.Log;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.util.HashMap;
+
+public class MapHandler extends DefaultHandler
+{
+    private boolean inTileSet, inTile, inLayer, inData, inObjectGroup, inProperties;
+
+    private String currentTileID;
+    private String currentObjectGroupName;
+
+    private MapData.MapObject currentObject;
+    private MapData.TileSet currentTileSet;
+    private MapData.Layer currentLayer;
+
+    private HashMap<String, HashMap<String, String>> currentTileSetProperties;
+    private HashMap<String, String> currentLayerProperties;
+
+    private MapData data;
+
+    private char[] buffer;
+    private int bufferIndex;
+    private int currentX;
+    private int currentY;
+    private static final int MAX_INT_DECIMAL_LENGTH = 10;
+
+    MapHandler()
+    {
+        super();
+        buffer = new char[MAX_INT_DECIMAL_LENGTH];
+        bufferIndex = 0;
+        currentX = 0;
+        currentY = 0;
+    }
+
+    MapData getData()
+    {
+        return data;
+    }
+
+    @Override
+    public void startDocument()
+    {
+        data = new MapData();
+    }
+
+    @Override
+    public void endDocument()
+    {
+
+    }
+
+    @Override
+    public void startElement(String namespaceURI, String localName, String qName, Attributes attributes) throws SAXException
+    {
+        switch (localName)
+        {
+            case "map":
+            {
+                if(!(attributes.getValue("orientation").equals("orthogonal")))
+                {
+                    throw new SAXException("Unsupported orientation. Parse Terminated.");
+                }
+
+                data.orientation = attributes.getValue("orientation");
+                Log.d("Checking", data.orientation);
+                data.height = Integer.parseInt(attributes.getValue("height"));
+                data.width = Integer.parseInt(attributes.getValue("width"));
+                data.tileWidth = Integer.parseInt(attributes.getValue("tilewidth"));
+                data.tileHeight = Integer.parseInt(attributes.getValue("tileheight"));
+            }
+            break;
+
+            case "tileset":
+            {
+                inTileSet = true;
+                currentTileSet = new MapData.TileSet();
+                currentTileSet.firstGID = Integer.parseInt(attributes.getValue("firstgid"));
+                currentTileSet.tileWidth = Integer.parseInt(attributes.getValue("tilewidth"));
+                currentTileSet.tileHeight = Integer.parseInt(attributes.getValue("tileheight"));
+                currentTileSet.name = attributes.getValue("name");
+                currentTileSetProperties = new HashMap<>();
+            }
+            break;
+
+            case "image":
+            {
+                if(inTileSet)
+                {
+                    currentTileSet.imageFilename = attributes.getValue("source");
+                    currentTileSet.imageWidth = Integer.parseInt(attributes.getValue("width"));
+                    currentTileSet.imageHeight = Integer.parseInt(attributes.getValue("height"));
+                }
+            }
+            break;
+
+            case "tile":
+            {
+                if(inTileSet)
+                {
+                    inTile = true;
+                    currentTileID = attributes.getValue("id");
+                }
+            }
+            break;
+
+            case "properties":
+            {
+                inProperties = true;
+
+                if (inTile)
+                {
+                    Log.d("Tile ID", currentTileID);
+                    currentTileSetProperties.put(currentTileID, new HashMap<String, String>());
+                }
+            }
+            break;
+
+            case "property":
+            {
+                if(inTile && inProperties)
+                {
+                    HashMap <String, String> properties = currentTileSetProperties.get(currentTileID);
+
+                    if(properties != null)
+                    {
+                        properties.put(attributes.getValue("name"), attributes.getValue("value"));
+                    }
+                    else
+                    {
+                        Log.d("Properties NULL", currentTileID);
+                    }
+                }
+                else if(inLayer && inProperties)
+                {
+                    currentLayerProperties.put(attributes.getValue("name"), attributes.getValue("value"));
+                }
+            }
+            break;
+
+            case "layer":
+            {
+                inLayer = true;
+
+                currentLayer = new MapData.Layer();
+                currentLayer.name = attributes.getValue("name");
+                currentLayer.width = Integer.parseInt(attributes.getValue("width"));
+                currentLayer.height = Integer.parseInt(attributes.getValue("height"));
+                if(attributes.getValue("opacity") != null) currentLayer.opacity = Double.parseDouble(attributes.getValue("opacity"));
+                currentLayer.tiles = new long[currentLayer.height][currentLayer.width];
+
+                currentLayerProperties = new HashMap<>();
+            }
+            break;
+
+            case "data":
+            {
+                inData = true;
+                String encoding = attributes.getValue("encoding");
+                if(!encoding.equals("csv"))
+                {
+                    throw new SAXException("Unsupported encoding. Parse Terminated.");
+                }
+            }
+            break;
+
+            case "objectGroup":
+            {
+                inObjectGroup = true;
+                currentObjectGroupName = attributes.getValue("name");
+            }
+            break;
+
+            case "object":
+            {
+                currentObject = new MapData.MapObject();
+                currentObject.name = attributes.getValue("name");
+                currentObject.type = attributes.getValue("type");
+                currentObject.x = Integer.parseInt(attributes.getValue("x"));
+                currentObject.y = Integer.parseInt(attributes.getValue("y"));
+                currentObject.width = Integer.parseInt(attributes.getValue("width"));
+                currentObject.height = Integer.parseInt(attributes.getValue("height"));
+                if(inObjectGroup)
+                {
+                    currentObject.objectGroup = currentObjectGroupName;
+                }
+                else
+                {
+                    currentObject.objectGroup = null;
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    public void endElement(String namespaceURI, String localName, String qName)
+    {
+        switch(localName)
+        {
+            case "map": break;
+
+            case "tileset":
+            {
+                inTileSet = false;
+                currentTileSet.properties = currentTileSetProperties;
+                currentTileSetProperties = null;
+                data.tileSets.add(currentTileSet);
+                currentTileSet = null;
+            }
+            break;
+
+            case "tile":
+            {
+                inTile = false;
+                currentTileID = "-1";
+            }
+            break;
+
+            case "properties": inProperties = false; break;
+
+            case "layer":
+            {
+                inLayer = false;
+                currentLayer.properties = currentLayerProperties;
+                data.layers.add(currentLayer);
+                currentLayer = null;
+            }
+            break;
+
+            case "data":
+            {
+                inData = false;
+
+                if (bufferIndex > 0)
+                {
+                    currentLayer.tiles[currentY][currentX] = Long.parseLong(new String(buffer, 0, bufferIndex));
+                }
+
+                bufferIndex = 0;
+                currentX = 0;
+                currentY = 0;
+            }
+            break;
+
+            case "objectGroup": inObjectGroup = false; break;
+
+            case "object": data.objects.add(currentObject); break;
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length)
+    {
+        if (inData)
+        {
+            for(int i = 0; i < length; i++)
+            {
+                if(ch[start+i] <= '9' && ch[start+i] >= '0')
+                {
+                    buffer[bufferIndex] = ch[start+i];
+                    bufferIndex++;
+                }
+                else
+                {
+                    String nextNumber = new String(buffer, 0, bufferIndex);
+                    if(!nextNumber.trim().equals("") && (bufferIndex != 0))
+                    {
+                        currentLayer.tiles[currentY][currentX] = Long.parseLong(nextNumber);
+                        bufferIndex = 0;
+
+                        if(currentX < (currentLayer.width - 1))
+                        {
+                            currentX++;
+                        }
+                        else if(currentY < (currentLayer.height - 1))
+                        {
+                            currentX = 0;
+                            currentY++;
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/gatavprojekt_001/MainGameActivity.java
+++ app/src/main/java/com/example/gatavprojekt_001/MainGameActivity.java
@@ -1,21 +1,61 @@
 package com.example.gatavprojekt_001;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainGameActivity extends AppCompatActivity {
+import com.example.gatavprojekt_001.Background_Layer.MapData;
+import com.example.gatavprojekt_001.Background_Layer.MapLoader;
 
-    private ImageView map;
-    final String filename = "test1.tmx";
+public class MainGameActivity extends AppCompatActivity
+{
+    private static final String FILE_PATH = "maps/test1.tmx";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_game);
+
+        MapData data = MapLoader.ReadTMXFile(FILE_PATH, this);
+
+        ImageView map = findViewById(R.id.Map);
+
+        Bitmap mapImage = MapLoader.CreateBitmap(data, this, 0, data.layers.size());
+
+        mapImage = scaleBitmap(mapImage, this);
+
+        if(mapImage != null)
+        {
+            map.setImageBitmap(mapImage);
+        }
+        else
+        {
+            Toast errorMessage = Toast.makeText(getApplicationContext(), "Map could not be loaded", Toast.LENGTH_LONG);
+            errorMessage.show();
+        }
+    }
+
+    private static Bitmap scaleBitmap(Bitmap map, Context context)
+    {
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+
+        Point size = new Point();
+        display.getSize(size);
+
+        int displayWidth = size.x;
+        int displayHeight = size.y;
+
+        return Bitmap.createScaledBitmap(map, displayWidth, displayHeight, true);
     }
 }
